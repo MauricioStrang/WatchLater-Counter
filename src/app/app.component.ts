e2cc7aaa4f0149961c40e827460506935d5dd9a4
MauricioStrang <strangmauricio@gmail.com>
2025-08-07 17:52:44 -0300

    don't know what is going on, starting again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,12 @@
 import { Component } from '@angular/core';
-import { YoutubeAuthService } from './youtube-auth.service';
-import { YoutubeService } from './youtube.service';
-import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   count: number | null = null;
-
-  constructor(
-    private authService: YoutubeAuthService,
-    private youtubeService: YoutubeService
-  ) {}
-
-  async signIn() {
-    await this.authService.signIn();
-  }
-
-  async fetchWatchLater() {
-    this.count = await this.youtubeService.getWatchLaterCount();
-  }
 }
